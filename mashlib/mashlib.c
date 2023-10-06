6bd6e7a8d52dcd4853a719d6af546aa12283ce2b
Delta Mike <deltamichael@protonmail.com>
2023-10-06 23:18:09 +0300

    Support negative numbers for printing in mashlib

--- mashlib/mashlib.c
+++ mashlib/mashlib.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <stdbool.h>
 
-void print_num(uint64_t x) {
+void print_num(int64_t x) {
+	bool neg = false;
+	if (x < 0) {
+		neg = true;
+		x *= -1;
+	}
 	char buf[32];
 	size_t buf_sz = 1;
 	buf[sizeof(buf) - buf_sz] = '\n';
@@ -11,6 +17,10 @@ void print_num(uint64_t x) {
 		buf_sz++;
 		x /= 10;
 	} while(x);
+	if (neg) {
+		buf[sizeof(buf) - buf_sz - 1] = '-';
+		buf_sz++;
+	}
 	write(1, &buf[sizeof(buf) - buf_sz], buf_sz);
 }
 
--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 BARE_PATH="${1%.*}"
 MASHLIB_DIR="/lib64"
 MASHLIB_PATH=$MASHLIB_DIR"/mashlib.so"
-./mishmash "${BARE_PATH}.msh" && as "${BARE_PATH}.s" -o "${BARE_PATH}.o"
+./mishmash "${BARE_PATH}.msh" && as -g "${BARE_PATH}.s" -o "${BARE_PATH}.o"
 ld --dynamic-linker=/lib64/ld-linux-x86-64.so.2 "${BARE_PATH}.o" $MASHLIB_PATH -o $BARE_PATH
 # export LD_LIBRARY_PATH=$MASHLIB_PATH
 exec $BARE_PATH
