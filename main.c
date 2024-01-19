826fa99b8ed96621dc597990868b4679a4a42a72
Delta Mike <deltamichael@protonmail.com>
2024-01-19 13:16:56 +0200

    Free ast on parser error

--- main.c
+++ main.c
@@ -78,6 +78,7 @@ int main(int argc, char **argv)
 		free_list(tokens, free_token);
 		free_lexer(lexer);
 		free_parser(parser);
+		free_ast_stmt(stmt);
 		fprintf(stderr, "Compilation error\n");
 		return 1;
 	}
@@ -86,13 +87,10 @@ int main(int argc, char **argv)
 	SYM_TABLE* table = init_symtable(NULL);
 	quad_from_stmt(stmt, quads, table);
 
-	// LEAKS
 	ASM_GENERATOR* asm_gen = init_asm_generator(quads, table);
 	ag_generate_code(asm_gen);
 	char* out = ag_get_code(asm_gen);
 	output_source_to_file(file_path, out);
-	// LEAKS
-
 
 	if (debug_output) {
 		// debug output
